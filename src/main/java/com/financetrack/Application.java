0c857820bad624bc13ae158c4df5a7d8a29a1df4
Ricardo <rick_ferry1@hotmail.com>
2024-07-21 12:50:35 -0300

    Starter

--- src/main/java/com/financetrack/Application.java
+++ src/main/java/com/financetrack/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceTrackApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinanceTrackApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
